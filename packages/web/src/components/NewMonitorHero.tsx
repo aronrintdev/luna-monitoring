243a070578ace3c3ee122e15d3a01ff8fcfddac2
aronrint <aronrint@gmail.com>
2022-06-13 18:31:48 -0700

    complete new monitors page

--- packages/web/src/components/NewMonitorHero.tsx
+++ packages/web/src/components/NewMonitorHero.tsx
@@ -1,28 +1,28 @@
-import { Center, Box, Text, Heading, Button, VStack } from '@chakra-ui/react'
+import { Center, VStack, Box } from '@chakra-ui/react'
 import { useNavigate } from 'react-router-dom'
+import PrimaryButton from './PrimaryButton'
+import Text from './Text'
+import Section from './Section'
+import RafikiSvg from '../assets/rafiki.svg'
 
 export function NewMonitorHero() {
   const navigate = useNavigate()
 
   return (
-    <Center h='50vh'>
-      <VStack spacing='12'>
-        <Text fontSize='6xl'>
-        <Text
-            as='span'
-            fontFamily='Kontora, Poppins, Arial, "Helvetica Neue", Helvetica, sans-serif'
-            fontWeight='extrabold'
-            color='#2cb67d'
-            borderBottom='7px solid'
-          >
-            Explore API and create 
-          </Text>
-          <Text as='span'>&nbsp;a new monitor</Text>
-        </Text>
-        <Button size='lg' colorScheme='blue' onClick={() => navigate('/console/monitors/newapi')}>
-          New Monitor
-        </Button>
-      </VStack>
-    </Center>
+    <Section mb={0}>
+      <Center h='calc(100vh - 5.5em)'>
+        <VStack spacing='8'>
+          <img src={RafikiSvg} width='620px' alt="new monitor hero page" />
+          <Text variant='header' color='black'>Explore API and create a new monitor</Text>
+          <PrimaryButton
+            label='New Monitor'
+            variant='emphasis'
+            color='white'
+            onClick={() => navigate('/console/monitors/newapi')}
+          ></PrimaryButton>
+          <Box h={25}></Box>
+        </VStack>
+      </Center>
+    </Section>
   )
 }
