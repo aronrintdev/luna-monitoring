b35f2a74505b9bbede8ba6aaadb15b0e0aaf757c
aronrint <aronrint@gmail.com>
2022-07-02 11:19:49 -0700

    fix type checking add vite typechecking plugin

--- packages/web/src/components/DatePicker/DatePicker.tsx
+++ packages/web/src/components/DatePicker/DatePicker.tsx
@@ -10,22 +10,22 @@ import './date-picker.css'
  *
  */
 
-export function DatePicker(props: ReactDatePickerProps) {
-  const { isClearable = false, showPopperArrow = false, ...rest } = props
-  const isLight = useColorMode().colorMode === 'light' //you can check what theme you are using right now however you want
+// export function DatePicker(props: ReactDatePickerProps) {
+//   const { isClearable = false, showPopperArrow = false, ...rest } = props
+//   const isLight = useColorMode().colorMode === 'light' //you can check what theme you are using right now however you want
 
-  return (
-    // if you don't want to use chakra's colors or you just wwant to use the original ones,
-    // set className to "light-theme-original" ↓↓↓↓
-    <div className={isLight ? 'light-theme' : 'dark-theme'}>
-      <ReactDatePicker
-        isClearable={isClearable}
-        showPopperArrow={showPopperArrow}
-        //input is white by default and there is no already defined class for it so I created a new one
-        className='react-datapicker__input-text'
-        selectsRange={true}
-        {...rest}
-      />
-    </div>
-  )
-}
+//   return (
+//     // if you don't want to use chakra's colors or you just wwant to use the original ones,
+//     // set className to "light-theme-original" ↓↓↓↓
+//     <div className={isLight ? 'light-theme' : 'dark-theme'}>
+//       <ReactDatePicker
+//         isClearable={isClearable}
+//         showPopperArrow={showPopperArrow}
+//         //input is white by default and there is no already defined class for it so I created a new one
+//         className='react-datapicker__input-text'
+//         selectsRange={true}
+//         {...rest}
+//       />
+//     </div>
+//   )
+// }
